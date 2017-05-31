b8e737eede5548be5767cecb5e2fd8f09fb1ed90
JonCass <JCass1980@gmail.com>
2017-05-31 15:38:12 -0700

    Ordinal Suffix Component In place and Functional

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -5,10 +5,10 @@ import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
+    {' · '}
     <Link to='/OrdinalSuffix' activeClassName='page-layout__nav-item--active'>Ordinal Suffix</Link>
     <div className='page-layout__viewport'>
       {children}
--- src/routes/OrdinalSuffix/components/OrdinalSuffix.js
+++ src/routes/OrdinalSuffix/components/OrdinalSuffix.js
@@ -1,10 +1,44 @@
 import React from 'react'
-import './OrdinalSuffix.scss'
+import './OrdinalSuffix.scss' 
 
-export const OrdinalSuffix = () => (
-  <div>
-    <h4>Ordinal Suffix</h4>
-  </div>
-)
+class OrdinalSuffix extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      testNumber : 0  
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  suffix(i) {
+    var j = i % 10,
+    k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+  
+  render () {
+    return (
+      <div>
+        <h4>Ordinal Suffix</h4>
+        <p>Enter a Number and observe the proper ordinal suffix be automatically affixed!</p>
+        <input type="text" name="testNumber" placeholder="Enter Your Number" onChange={this.handleChange} value={this.state.testNumber} className="inputBox"></input>
+        <p>{this.suffix(this.state.testNumber)}</p>
+      </div>
+    )
+  }
+}
 
 export default OrdinalSuffix
