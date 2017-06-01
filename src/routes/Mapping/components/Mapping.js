f4ce269ab97e9ba1f51a66464f871bb1e37539b9
JonCass <JCass1980@gmail.com>
2017-06-01 11:13:30 -0700

    Mapping component complete

--- src/routes/Mapping/components/Mapping.js
+++ src/routes/Mapping/components/Mapping.js
@@ -5,16 +5,33 @@ class Mapping extends React.Component {
   constructor() {
     super()
     this.state = {
-      testArray : ["This", "Is", "A", "Simple", "Array", "To", "Map", "Though", "And", "Display"]  
+      testArray : ["This", "Is", "A", "Simple", "Array", "To", "Map", "Though", "And", "Display", "."]  
     }
   }
+  
+  mapFunction(){      //Relocated from render to allow if statements
+    if(this.state.testArray) {      // this would check if the props were defined, here we just put the array from state in as a placeholder
+      return (
+        this.state.testArray.map( (entry, i) =>(  //The index i is here and can be used to label the key for the mapping
+          <p key = {i} className = "mapEntry" >{ entry } </p>
+        ))
+      )
+    }else {
+      return (
+        <span> Loading Props. </span>
+      )
+    }
+  }
+
   render () {
     return (
-      <div>
-        <h4>Mapping</h4>
+      <div> 
+        {/*Here we call the mapFunction into the Render to display it's contents*/}
+        { this.mapFunction() }
       </div>
     )
   }
+
 }
 
 export default Mapping
