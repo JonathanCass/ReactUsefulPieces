44e9f876870e2a52a4915841ef56d13c5923faf3
JonCass <JCass1980@gmail.com>
2017-06-01 14:02:05 -0700

    Finding Component in place

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ This is a simple project created with React Redux Starter Kit.
 The purpose of which is to hold simple components performing basic operations for future reference.  
 Components will perform actions such as mapping through and displaying an array or formatting and redisplaying information in a particular format.  
 The added components will all exist under src / routes .
+
+Components Present :  
+Ordinal Suffix [ Input , Applies appropriate ordinal suffix to inputted number ]
+Mapping [ Maps out an array that is present in initial state ]
+Filtering [ Submit numbers into an array , Displays : Array , Evens , Odds ]
+Finding [ User input is tested against an array and a string, displays position if found]
+    Case Insensitive version also present here
--- src/routes/Find/components/Find.js
+++ src/routes/Find/components/Find.js
@@ -7,7 +7,7 @@ class Find extends React.Component {
     this.state = {
       query : '',
       testArray : [ 'Dog', 'Cat', 'Turtle', 'Owl', 'Pony', 'Dragon', 'Griffon', 'Minotaur', 'Phoenix'],
-      testString : 'alphabravocharliedeltaechofoxtrotgulfhotelindiajulietkilomikenovember'  
+      testString : 'AlphaBravoCharlieDeltaEchoFoxtrotGulfHotelIndiaJulietKiloMikeNovember'  
     }
   }
   
@@ -24,28 +24,41 @@ class Find extends React.Component {
   }
   
   find (item , search) { 
-    if( item.indexOf(search) ==! -1){
-      console.log('item', item , 'search' , search)
-      console.log('item.indexOf(search)', item.indexOf(search))
+    if( item.indexOf(search) === -1){
       return(
-        <span>Query found at position {item.indexOf(search)}</span>
+        <span>Query not found</span>
       )
     }else{
       return(
-        <span>Query not found.</span>
+        <span>Postion : {item.indexOf(search)}</span>
       )
     }
   }
 
+  findNonCase (item , search) { 
+    if( item.findIndex(item => search.toLowerCase() === item.toLowerCase()) === -1){ // This is essentially a case insensitive version of indexOf for an array
+      return(
+        <span>Query not found</span>
+      )
+    }else{
+      return(
+        <span>Postion : {item.findIndex(item => search.toLowerCase() === item.toLowerCase())}</span>
+      )
+    }
+  }
+  
   render () {
     return (
       <div>
         <div>Test Array : { this.displayFunction(this.state.testArray) }</div>
         <div>Test String : { this.state.testString }</div>
         <input type ='text' name = 'query' onChange={this.handleChange} value ={this.state.query} placeholder ='Search for ?' className="inputBox" ></input>
-        <div> Searching for : {this.state.query} </div>
+        <div className="divLabel" > Searching for : {this.state.query} </div>
         <div> Array : { this.find( this.state.testArray , this.state.query) } </div>
         <div> String : { this.find( this.state.testString , this.state.query) } </div>
+        <div className="divLabel"> Case Insensitive </div>
+        <div> Array : { this.findNonCase( this.state.testArray , this.state.query) } </div>
+        <div> String : { this.find( this.state.testString.toLowerCase() , this.state.query.toLowerCase()) } </div>
       </div>
     )
   }
